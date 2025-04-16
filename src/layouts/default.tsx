d597a1e043cbc22fb129b5c2474638e2b300a1ce
Luqni Maulana <37354232+luqnimaula@users.noreply.github.com>
2025-04-16 23:04:52 +0700

    fix: auth settings

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -6,22 +6,23 @@ import { useEffect } from 'react'
 import { useProfile } from '@/hooks/profile'
 import { BACKOFFICE_URL, getAuthHeaders, SESSION_KEY } from '@/config/constants'
 import { useRouter } from 'next/router'
+import { Spinner } from '@heroui/react'
 
 const redirectAuthURL = `${BACKOFFICE_URL}/auth-check?external=1`
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter()
-  const { profileStatus } = useProfile()
+  const { loading, profileStatus } = useProfile()
 
   useEffect(() => {
-    if (router.pathname.startsWith('/settings')) {
+    if (!loading && router.pathname.startsWith('/settings')) {
       (async () => {
         const headers = await getAuthHeaders()
         const is_has_session = !!headers?.[SESSION_KEY.header]
 
         if (is_has_session) {
           if (profileStatus === 401) {
-            window.location.href = redirectAuthURL
+            // window.location.href = redirectAuthURL
             return
           }
         } else {
@@ -30,7 +31,15 @@ export default function DefaultLayout({ children }: { children: React.ReactNode 
         }
       })()
     }
-  }, [profileStatus, router.pathname])
+  }, [profileStatus, router.pathname, loading])
+
+  if (loading) {
+    return (
+      <div className='w-full min-h-[100dvh] flex items-center justify-center'>
+        <Spinner size='lg' />
+      </div>
+    )
+  }
 
   return (
     <div className='relative flex flex-col min-h-[100dvh]'>
