7e61d34e15e80e1c0eeb6df7d4befc4f154064dc
Luqni Maulana <37354232+luqnimaula@users.noreply.github.com>
2025-04-26 21:35:01 +0700

    fix: adaptive slider height & optimize logics

--- src/components/DashboardWithSlider.tsx
+++ src/components/DashboardWithSlider.tsx
@@ -1,4 +1,4 @@
-import { memo, useMemo, useRef } from 'react'
+import { memo, useMemo, useRef, useState } from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
@@ -8,17 +8,6 @@ import clsx from 'clsx'
 import RenderCharts from './RenderCharts'
 
 import { ChartItem, DashboardSettingItem } from '@/types/dashboard-settings'
-
-const settings = {
-  dots: false,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 1000,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false
-}
 
 const arrowStyles =
   'm-auto cursor-pointer border-foreground-200 border-2 bg-background/70 hover:bg-background transition-all ease-in-out duration-150 shadow-xl flex w-10 aspect-square rounded-full z-10'
@@ -30,49 +19,61 @@ type Props = {
 
 const DashboardWithSlider: React.FC<Props> = ({ dashboard, charts }) => {
   const refSlider = useRef<Slider | null>(null)
-
-  const sortedCharts = useMemo(() => {
-    return charts
-      .sort((a, b) => Number(a.chart_slide) - Number(b.chart_slide))
-      .sort((a, b) => Number(a.chart_order) - Number(b.chart_order))
-  }, [charts])
+  const [currentSlide, setCurrentSlide] = useState<number>(0)
 
   const groupedChartsBySlide = useMemo(() => {
-    return sortedCharts.reduce(
-      (acc, chart) => {
-        const slideIndex = Number(chart.chart_slide) - 1
+    const grouped = charts
+      .sort((a, b) => Number(a.chart_slide) - Number(b.chart_slide))
+      .sort((a, b) => Number(a.chart_order) - Number(b.chart_order))
+      .reduce(
+        (acc, chart) => {
+          const slideIndex = Number(chart.chart_slide) - 1
 
-        if (!acc[slideIndex]) {
-          acc[slideIndex] = []
-        }
-        acc[slideIndex].push(chart)
+          if (!acc[slideIndex]) {
+            acc[slideIndex] = []
+          }
+          acc[slideIndex].push(chart)
 
-        return acc
-      },
-      {} as Record<number, ChartItem[]>
-    )
-  }, [sortedCharts])
+          return acc
+        },
+        {} as Record<number, ChartItem[]>
+      )
+
+    return Object.values(grouped)
+  }, [charts])
+
+  const settings = useMemo(
+    () => ({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      autoplay: !!dashboard?.dashboard_slide_timer,
+      autoplaySpeed: dashboard?.dashboard_slide_timer ? Number(dashboard?.dashboard_slide_timer) * 60000 : 1000,
+      beforeChange: (_current: number, next: number) => setCurrentSlide(next)
+    }),
+    [dashboard]
+  )
 
   return (
     <div className='w-full relative'>
-      <div className={clsx('absolute left-2 h-full flex', Object.keys(groupedChartsBySlide).length <= 1 && 'hidden')}>
+      <div className={clsx('absolute left-2 h-full flex', groupedChartsBySlide.length <= 1 && 'hidden')}>
         <div role='button' onClick={() => refSlider.current?.slickPrev()} className={arrowStyles}>
           <ChevronLeft className='m-auto' />
         </div>
       </div>
-      <div className={clsx('absolute right-2 h-full flex', Object.keys(groupedChartsBySlide).length <= 1 && 'hidden')}>
+      <div className={clsx('absolute right-2 h-full flex', groupedChartsBySlide.length <= 1 && 'hidden')}>
         <div role='button' onClick={() => refSlider.current?.slickNext()} className={arrowStyles}>
           <ChevronRight className='m-auto' />
         </div>
       </div>
-      <Slider
-        ref={refSlider}
-        {...settings}
-        autoplay={!!dashboard?.dashboard_slide_timer}
-        autoplaySpeed={dashboard?.dashboard_slide_timer ? Number(dashboard?.dashboard_slide_timer) * 60000 : undefined}
-      >
-        {Object.values(groupedChartsBySlide).map((charts, i) => (
-          <RenderCharts key={i} charts={charts} wrapperClassName='px-16 space-y-6 pb-4' />
+      <Slider ref={refSlider} {...settings}>
+        {groupedChartsBySlide.map((charts, i) => (
+          <div key={i} className='w-full'>
+            {currentSlide === i && <RenderCharts charts={charts} wrapperClassName='px-16 space-y-6 pb-4' />}
+          </div>
         ))}
       </Slider>
     </div>
