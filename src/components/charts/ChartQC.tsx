d2dc0ba875cc7eed101b79b2dfabf9914e78e775
Luqni Maulana <37354232+luqnimaula@users.noreply.github.com>
2025-05-21 20:54:15 +0700

    feat: show/hide filter for chart qc

--- src/components/charts/ChartQC.tsx
+++ src/components/charts/ChartQC.tsx
@@ -35,7 +35,7 @@ import {
   QCParametersFilterStateProperties
 } from '@/types/chart'
 import { buildChartFilters } from '@/utils/chart-filters'
-import { useChartFilter } from '@/hooks/chart-filter'
+import { useChartFilter, useChartFilterVisibility } from '@/hooks/chart-filter'
 import toast from 'react-hot-toast'
 import CategoryFilter from './filters/CategoryFilter'
 import ChartTypeFilter from './filters/ChartTypeFilter'
@@ -80,6 +80,9 @@ const ChartQC: React.FC<Props> = ({ chart }) => {
   const [part3Data, setPart3Data] = useState<ChartQCPart3Data | null>(null)
 
   const [chartNames, setChartNames] = useState<Record<number, string>>({})
+  const [chartNamesHiddenFilter, setChartNamesHiddenFilter] = useState<Record<number, string>>({})
+
+  const { showFilter } = useChartFilterVisibility()
 
   const { loading, loadingChart, chartData, getDisplayChart } = useDisplayChart<{
     chart: {
@@ -257,24 +260,27 @@ const ChartQC: React.FC<Props> = ({ chart }) => {
           ).chart
 
           setPart1Data(chart)
+          setChartNamesHiddenFilter((current) => ({ ...current, 1: chart?.[0]?.['text-filter'] || '' }))
         }
         if (part === '2') {
-          setPart2Data(
-            (
-              data as {
-                chart: ChartQCPart2Data
-              }
-            ).chart
-          )
+          const chart = (
+            data as {
+              chart: ChartQCPart2Data
+            }
+          ).chart
+
+          setPart2Data(chart)
+          setChartNamesHiddenFilter((current) => ({ ...current, 2: chart?.['text-filter'] || '' }))
         }
         if (part === '3') {
-          setPart3Data(
-            (
-              data as {
-                chart: ChartQCPart3Data
-              }
-            ).chart
-          )
+          const chart = (
+            data as {
+              chart: ChartQCPart3Data
+            }
+          ).chart
+
+          setPart3Data(chart)
+          setChartNamesHiddenFilter((current) => ({ ...current, 3: chart?.['text-filter'] || '' }))
         }
       }
     }
@@ -398,7 +404,7 @@ const ChartQC: React.FC<Props> = ({ chart }) => {
                 onSubmitChartPart1()
               }}
             >
-              <div className='w-full'>
+              <div className={clsx('w-full', !showFilter && 'hidden')}>
                 <Select
                   className='max-w-xs'
                   label={parametersProperties?.label_name}
@@ -417,7 +423,7 @@ const ChartQC: React.FC<Props> = ({ chart }) => {
                   ))}
                 </Select>
               </div>
-              <div className='w-full space-y-1'>
+              <div className={clsx('w-full space-y-1', !showFilter && 'hidden')}>
                 <div className='w-full flex items-center gap-2'>
                   <Select
                     className='w-full max-w-[13rem]'
@@ -526,7 +532,7 @@ const ChartQC: React.FC<Props> = ({ chart }) => {
                   </div>
                 </div>
               </div>
-              <div className='w-full inline-flex items-center gap-2'>
+              <div className={clsx('w-full inline-flex items-center gap-2', !showFilter && 'hidden')}>
                 <DateRangePicker
                   showMonthAndYearPickers
                   className='max-w-xs'
@@ -587,13 +593,15 @@ const ChartQC: React.FC<Props> = ({ chart }) => {
                 <ChartTypeFilter state={chartTypeProperties} value={chartTypeValue} onChange={setChartTypeValue} />
               </div>
               <div className='w-full flex justify-between items-center gap-2'>
-                <div className='text-xl font-semibold'>{chartNames?.[1]}</div>
+                <div className='text-xl font-semibold'>
+                  {showFilter ? chartNames?.[1] : chartNamesHiddenFilter?.[1]}
+                </div>
                 <Button
                   type='submit'
                   id={`submit-part1-${chart.id}`}
                   color='primary'
                   isLoading={loadingChart}
-                  className='print:hidden'
+                  className={clsx(!showFilter && 'hidden', 'print:hidden')}
                 >
                   Tampilkan
                 </Button>
@@ -611,8 +619,10 @@ const ChartQC: React.FC<Props> = ({ chart }) => {
             <Card className='w-full space-y-2 print:shadow-none break-inside-avoid-page'>
               <CardHeader>
                 <div className='w-full space-y-3'>
-                  <div className='text-xl font-semibold'>{chartNames?.[2]}</div>
-                  <div className='w-full'>
+                  <div className='text-xl font-semibold'>
+                    {showFilter ? chartNames?.[2] : chartNamesHiddenFilter?.[2]}
+                  </div>
+                  <div className={clsx('w-full', !showFilter && 'hidden')}>
                     <CategoryFilter properties={categoryProperties} onChangeFilters={onChangeCategoryFilters} />
                   </div>
                 </div>
@@ -624,8 +634,10 @@ const ChartQC: React.FC<Props> = ({ chart }) => {
             <Card className='w-full space-y-2 print:shadow-none break-inside-avoid-page'>
               <CardHeader>
                 <div className='w-full space-y-3'>
-                  <div className='text-xl font-semibold'>{chartNames?.[3]}</div>
-                  <div className='w-full'>
+                  <div className='text-xl font-semibold'>
+                    {showFilter ? chartNames?.[3] : chartNamesHiddenFilter?.[3]}
+                  </div>
+                  <div className={clsx('w-full', !showFilter && 'hidden')}>
                     <Select
                       className='max-w-xs'
                       label={numericProperties?.label_name}
