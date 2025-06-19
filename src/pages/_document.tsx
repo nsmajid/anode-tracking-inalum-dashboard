41e3634e7746f6e22cacf17090bd6cfd44ba83bc
Luqni Maulana <37354232+luqnimaula@users.noreply.github.com>
2025-06-19 22:50:37 +0700

    fix: force mobile device to use desktop resolution

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,11 @@ import { fontSans } from '@/config/fonts'
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
-      <body className={clsx('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+      <Head>
+        {/* Force desktop viewport width on mobile */}
+        <meta name='viewport' content='width=1024' />
+      </Head>
+      <body className={clsx('min-h-screen bg-background font-sans antialiased at-layout', fontSans.variable)}>
         <Main />
         <NextScript />
       </body>
